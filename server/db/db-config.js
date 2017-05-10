92b4610359178f650c6bbee71850dcedc609acbd
dppedersen <pedersen.duncan@gmail.com>
2017-05-09 20:05:13 -0400

    Begin Implementing savedJobs Widget

--- server/db/db-config.js
+++ server/db/db-config.js
@@ -2,7 +2,15 @@ var mongoose = require('mongoose');
 
 // connect to database at europaDB
 // Or, use ours mongodb://europa:europa@ds133331.mlab.com:33331/europa
+<<<<<<< HEAD
 var dbURI = 'mongodb://demo:demo@ds045694.mlab.com:45694/legacy';
+=======
+<<<<<<< HEAD
+var dbURI = 'mongodb://europa:europa@ds133331.mlab.com:33331/europa';
+=======
+var dbURI = 'mongodb://demo:demo@ds045694.mlab.com:45694/legacy';
+>>>>>>> Begin Implementing savedJobs Widget
+>>>>>>> Begin Implementing savedJobs Widget
 mongoose.connect(dbURI);
 
 var db = mongoose.connection;
